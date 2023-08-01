6552446bf28bc95ecfa39f4615b95b6c85a6147f
onusra@angelo.edu <onusra@angelo.edu>
2023-08-01 17:19:29 -0500

    create a project

---
+++ src/main/java/com/myscedular/org/ombeninusra/OmbeninusraApplication.java
@@ -0,0 +1,13 @@
+package com.myscedular.org.ombeninusra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmbeninusraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmbeninusraApplication.class, args);
+	}
+
+}
